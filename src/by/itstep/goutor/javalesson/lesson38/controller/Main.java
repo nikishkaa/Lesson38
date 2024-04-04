d8a95f429dc818d46d187e6b69cd20322037c612
nikishkaa <nikita.goutor@gmail.com>
2024-04-04 20:19:59 +0300

    Initial commit

---
+++ src/by/itstep/goutor/javalesson/lesson38/controller/Main.java
@@ -0,0 +1,10 @@
+package by.itstep.goutor.javalesson.lesson38.controller;
+
+import by.itstep.goutor.javalesson.lesson38.model.entity.Student;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+
+}
